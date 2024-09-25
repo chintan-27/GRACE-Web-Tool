469103ecb5d704f681ce9d148ce035358827be36
Chintan Acharya <chintan.acharya@bmes-iMac.local>
2024-09-25 12:37:29 -0400

    Converting pytorch model to ONNX

---
+++ python/convertPthToOnnx.py
@@ -0,0 +1,32 @@
+from monai.networks.nets import UNETR
+from monai.losses import DiceCELoss
+import torch
+args = {}
+
+args.N_classes = 12
+args.spatial_size = 128
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = UNETR(
+      in_channels=1,
+      out_channels=args.N_classes, #12 for all tissues
+      img_size=(args.spatial_size, args.spatial_size, args.spatial_size),
+      feature_size=16, 
+      hidden_size=768,
+      mlp_dim=3072,
+      num_heads=12,
+      pos_embed="perceptron",
+      norm_name="instance",
+      res_block=True,
+      dropout_rate=0.0,
+    ).to(device)
+
+loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+torch.backends.cudnn.benchmark = True
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+model.load_state_dict(torch.load("models/grace.pth"))
+
+dummy_input = torch.randn(1, 1, 128, 128, 128).to(device)
+
+torch.onnx.export(model, dummy_input, "models/grace.onnx", opset_version=11)
