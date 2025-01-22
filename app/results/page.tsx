130d1b272da71054df8cfeeeec5bcf3b5eeaa864
Dev Patel <68773259+d3v-26@users.noreply.github.com>
2025-01-22 16:20:28 -0500

    Loading view updated

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -93,11 +93,10 @@ const Results = () => {
       
     } catch (error) {
       console.error("Inference error:", error);
-    } finally {
-      setInfLoading(false);
-    }
+    } 
 
     async function fetchOutput() {
+      
       const outputResponse = await fetch("http://localhost:5500/output")
       const inferredBlob = await outputResponse.blob();
       const inferredImage = await NVImage.loadFromFile({
@@ -105,38 +104,54 @@ const Results = () => {
         colormap:'jet',
         opacity: 1,
       });
-      
+      setInfLoading(true);
       setInferenceResults(inferredImage);
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
-      {loading ? (
-        <div className="flex items-center justify-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-lime-800"></div>
-        </div>
-      ) : (
-        <div>
-          <div className="p-4 bg-white shadow-md m-10 w-50">
-            {image && (
-              <NiiVue
-                image={image}
-                inferredImage={inferenceResults}
-              />
-            )}
+    <div>
+    
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-lime-800"></div>
           </div>
-          <div className="flex justify-center mt-4">
-            <button
-              className="bg-lime-800 hover:bg-lime-950 duration-200 text-white font-bold py-2 px-4 rounded"
-              onClick={handleInference}
-              disabled={infLoading}
-            >
-              {infLoading ? progress.message : "Inference Using API"}
-            </button>
+        ) : (
+          <div>
+            <div className="p-4 bg-white shadow-md m-10 w-50">
+              {image && (
+                <NiiVue
+                  image={image}
+                  inferredImage={inferenceResults}
+                />
+              )}
+            </div>
+            {infLoading ? 
+              <div className="flex justify-center mt-4">
+                <div className="w-1/2 block bg-gray-200 rounded-full dark:bg-gray-700">
+                  <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: progress.progress.toString()+"%"}}>{ progress.progress.toString()+"%"}</div>
+                </div>
+              </div>
+             : <span></span>}
+             <br/>
+            <div className="flex justify-center mt-4">
+              <button
+                className="bg-lime-800 hover:bg-lime-950 duration-200 text-white font-bold py-2 px-4 rounded"
+                onClick={handleInference}
+                disabled={infLoading}
+              >
+                {infLoading ? progress.message : "Inference Using API"}
+              </button>
+              
+              
+
+            </div>
           </div>
-        </div>
-      )}
+        )}
+        
+      </div>
+      
     </div>
   );
 };
