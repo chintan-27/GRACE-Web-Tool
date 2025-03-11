ed6bc7be6dcc3ab741dfaa004ff7bd3bcdb8386f
Dev Patel <68773259+d3v-26@users.noreply.github.com>
2025-03-11 16:24:36 -0400

    socket updated

--- api/app.py
+++ api/app.py
@@ -160,4 +160,4 @@ def dominoppOutput():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
