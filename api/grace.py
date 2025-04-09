b76b3b5231589ee049f9673ffadfb95c79b16d2a
chintan-27 <chintan.acharya27@gmail.com>
2025-04-08 20:08:43 -0400

    Rremoved Resizewithcropandpad

--- api/grace.py
+++ api/grace.py
@@ -124,7 +124,6 @@ def preprocess_input(input_path, device, a_min_value=0, a_max_value=255, complex
         Spacingd(keys=["image"], pixdim=(1.0, 1.0, 1.0), mode=("bilinear")),
         Orientationd(keys=["image"], axcodes="RAS"),
         CropForegroundd(keys=["image"], source_key="image"),
-        ResizeWithPadOrCropd(keys=["image"], spatial_size=(64, 64, 64))
     ])
 
     yield send_progress("Applying spatial transforms...", 40)
