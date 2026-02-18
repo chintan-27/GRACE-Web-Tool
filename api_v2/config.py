47e4f6e52a06c284553e89f5f5bbba0650a67200
chintan-27 <chintan.acharya27@gmail.com>
2026-02-18 11:25:07 -0500

    chore(roast): mount build from server path, ignore roast-11 source

--- api_v2/config.py
+++ api_v2/config.py
@@ -50,7 +50,7 @@ JOB_TIMEOUT_SECONDS = int(os.getenv("JOB_TIMEOUT_SECONDS", "3600"))
 # -------------------------------------------------------
 # ROAST CONFIG
 # -------------------------------------------------------
-ROAST_BUILD_DIR = Path(os.getenv("ROAST_BUILD_DIR", str(BASE_DIR.parent / "roast-11" / "build")))
+ROAST_BUILD_DIR = Path(os.getenv("ROAST_BUILD_DIR", "/home/chintan/roast11/build"))
 MATLAB_RUNTIME = Path(os.getenv("MATLAB_RUNTIME", "/usr/local/MATLAB/MATLAB_Runtime/R2025b"))
 ROAST_MAX_WORKERS = int(os.getenv("ROAST_MAX_WORKERS", "2"))
 ROAST_TIMEOUT_SECONDS = int(os.getenv("ROAST_TIMEOUT_SECONDS", "7200"))
